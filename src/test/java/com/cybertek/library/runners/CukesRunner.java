8e033ed98b9577da062b5bfdc53d8c0ab9d30c17
Daria <pogorelskykhdaria@gmail.com>
2021-07-27 18:01:46 -0500

    initial commit 4

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -7,7 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/reports/cucumber-report.html",
+        plugin = {
+                "html:target/reports/cucumber-report.html",
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
         dryRun = false,
